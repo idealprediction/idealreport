d126a2ccc52b68178661ff8c4d506ed9a37fcd2e
crouchideal <john@idealprediction.com>
2016-07-08 14:24:35 -0400

    Setup for distributing via PyPI

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'idealreport',
+  packages = ['idealreport'],
+  version = '0.1',
+  description = 'Ideal Prediction reporting framework',
+  author = 'Ideal Prediction',
+  author_email = 'info@idealprediction.com',
+  url = 'https://github.com/idealprediction/idealreport', 
+  download_url = 'https://github.com/idealprediction/idealreport/tarball/0.1', 
+  keywords = ['idealprediction', 'report'], 
+  classifiers = [],
+)
