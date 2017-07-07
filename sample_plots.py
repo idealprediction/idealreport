c4f6d66914ccf8a78b3fd056a5a61e91fa8fb320
jason <jason.mather@gatech.edu>
2017-07-07 14:52:49 -0400

    Added time series, secondary y axis example, and minimal comments.

--- sample_plots.py
+++ sample_plots.py
@@ -8,6 +8,8 @@ import idealreport.create_html as ch
 from idealreport.reporter import Reporter
 import htmltag
 
+# simple vertical bar chart, notice additional entry in the data field for orientation
+
 dfhb = DataFrame({'Entity': ['Entity 1', 'Entity 2', 'Entity 3', 'Entity 4', 'Entity 5'],
                   'Stat 1': np.random.randn(5).tolist(),
                   'Stat 2': np.random.randn(5).tolist(),
@@ -16,7 +18,7 @@ dfhb = dfhb.set_index('Entity')
 dfhb = dfhb.sort_values(by='Stat 1', ascending=False)
 
 bar_plot = {
-    'title': 'Sample Bar Chart by Create HTML',
+    'title': 'Vertical Bar Chart by Create HTML',
     'data': [
         {
             'df': dfhb,
@@ -29,6 +31,8 @@ bar_plot = {
     'staticPlot': False
 }
 
+# stacked horizontal bar chart, notice additional entry in the data field for orientation
+
 dfsb = DataFrame({'Entity': ['Entity 1', 'Entity 2', 'Entity 3', 'Entity 4', 'Entity 5'],
                 'Foo': [8, 10, 50, 85, 42],
                 'Bar': [100, 50, 10, 100, 25]})
@@ -36,7 +40,7 @@ dfsb = dfsb.set_index('Entity')
 dfsb = dfsb[['Foo', 'Bar']] # make sure columns have desired order
 
 stacked_bar_plot = {
-    'title': 'Sample Stacked Bar Chart by Create HTML',
+    'title': 'Horizontal Stacked Bar Chart by Create HTML',
     'data': [
         {
             'df': dfsb,
@@ -48,11 +52,30 @@ stacked_bar_plot = {
     'staticPlot': False
 }
 
-dfsp = DataFrame(np.random.randn(20, 2))
-dfsp.columns = ['a', 'b']
+# simple line chart
+
+dfl = DataFrame(np.random.randn(200, 3))
+dfl.columns = ['a', 'b', 'c']
+dfl.a = range(200)
+dfl = dfl.set_index('a')
+line_plot = {
+    'title': 'Line Plot by Create HTML',
+    'data': [
+        {
+            'df': dfl,
+            'type': 'line',
+        },
+    ],
+    'staticPlot': False
+}
+
+# scatter plot
+
+dfsp = DataFrame(np.random.randn(20, 3))
+dfsp.columns = ['a', 'b', 'c']
 dfsp = dfsp.set_index('a')
 scatter_plot = {
-    'title': 'Sample Scatter Plot by Create HTML',
+    'title': 'Scatter Plot by Create HTML',
     'data': [
         {
             'df': dfsp,
@@ -64,33 +87,65 @@ scatter_plot = {
     'staticPlot': False
 }
 
+# mixed plot types on one graph
+
 df1 = DataFrame(np.random.randn(200, 2))
 df1.columns = ['a', 'b1']
+df1.sort_values(by='a', ascending=True, inplace=True)
 df1 = df1.set_index('a')
+
 df2 = DataFrame(np.random.randn(200, 2))
 df2.columns = ['a', 'b2']
+df2.sort_values(by='a', ascending=True, inplace=True)
 df2 = df2.set_index('a')
-df3 = DataFrame(np.random.randn(200, 2))
-df3.columns = ['a', 'b3']
-df3 = df3.set_index('a')
+
+
 multi_scatter_plot = {
-    'title': 'Sample Scatter Plot with Multiple Series by Create HTML',
+    'title': 'Mixed Line and Bar Plot by Create HTML',
     'data': [
         {
             'df': df1,
-            'type': 'scatter',
+            'type': 'line',
         },
         {
             'df': df2,
-            'type': 'scatter',
-        },
-        {
-            'df': df3,
-            'type': 'scatter',
+            'type': 'bar',
+            'orientation': 'v',
         },
     ],
     'labelX': 'x label',
     'labelY': 'y label',
+    'staticPlot': False
+}
+
+#different axes
+
+dfa = DataFrame(np.random.randn(20, 2))
+dfa.columns = ['x', 'a']
+dfa.x = range(20)
+dfa = dfa.set_index('x')
+dfb = DataFrame(np.random.randn(20, 2))
+dfb.columns = ['x', 'b']
+dfb.x = range(20)
+dfb = dfb.set_index('x')
+dfb.b *= 100
+multi_axis_plot = {
+    'type': 'line',
+    'title': 'Multi-Axis Plot by Create HTML',
+    'data': [
+        {
+            'df': dfa,
+            'type': 'line',
+        },
+        {
+            'df': dfb,
+            'type': 'line',
+            'y2': True,
+        },
+    ],
+    'labelX': 'x',
+    'labelY': 'ya',
+    'labelY2': 'yb',
 }
 
 output_path = '/home/jason/ideal/reports/sample_report/output'
@@ -98,21 +153,31 @@ output_file = os.path.join(output_path, 'sample_plots.html')
 r = Reporter(title='Sample Plots', output_file=output_file)
 
 # report start
-r.h += htmltag.h3('This report illustrates some additional plotting types.')
+r.h += htmltag.h3('This report provides an overivew of the different plot types.')
 
 # bar charts
 r.h += htmltag.h4('Bar Charts')
-r.plot.bar(df=dfhb, title='Sample Bar Chart by Reporter', xlabel='Entity', ylabel='%')
+r.plot.bar(df=dfhb, title='Vertical Bar Chart by Reporter', xlabel='Entity', ylabel='%')
 r.h += ch.plot(bar_plot)
-r.plot.barh(df=dfsb, title='Sample Stacked Bar Chart by Reporter', stacked=True, xlabel='$')
+r.plot.barh(df=dfsb, title='Horizontal Stacked Bar Chart by Reporter', stacked=True, xlabel='$')
 r.h += ch.plot(stacked_bar_plot)
+
+# line charts
+r.h += htmltag.h4('Line Charts')
+r.plot.line(df=dfl, title='Line Chart by Reporter', xlabel='Entity', ylabel='%')
+r.h += ch.plot(line_plot)
 
 # scatter plots
 r.h += htmltag.h4('Scatter Plots')
-r.plot.scatter(df=dfsp, title='Sample Scatter Plot by Reporter', xlabel='alpha', ylabel='beta')
+r.plot.scatter(df=dfsp, title='Scatter Plot by Reporter', xlabel='alpha', ylabel='beta')
 r.h += ch.plot(scatter_plot)
-r.plot.multi(dfs=[df1, df2, df3], types=['scatter', 'scatter', 'scatter'], title='Sample Scatter Plot with Multiple Series by Reporter', xlabel='alpha', ylabel='beta')
+
+# mixed plot types
+r.h += htmltag.h4('Multi Series, Mixed Type Plots')
+r.plot.multi(dfs=[df1, df2], types=['line', 'bar'], title='Mixed Line and Bar Plot by Reporter', xlabel='x label', ylabel='y label')
 r.h += ch.plot(multi_scatter_plot)
+r.plot.multi(dfs=[dfa, dfb], types=['line', 'line'], title='Multi-Axis Plot by Reporter', xlabel='x', ylabel='ya', y2_axis=[False,True], y2label='yb')
+r.h += ch.plot(multi_axis_plot)
 
 # generate and save the report HTML
 r.generate()
